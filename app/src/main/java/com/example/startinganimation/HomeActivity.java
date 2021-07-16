ef31fbec27745bd977eb8cca229125b54ba53469
Ozzy <milkywaydebugger@gmail.com>
2021-07-16 04:39:59 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/startinganimation/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.startinganimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
